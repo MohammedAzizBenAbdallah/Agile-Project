f43f170ea3cfdb36b74ab1ccdc01257b7b1fb98f
islam10x <islamboubaker.7874@gmail.com>
2025-02-24 12:01:48 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Signup from './Signup'
+import './App.css'
+function App() {
+  return (
+    <div className='container'>
+      <Signup></Signup>
+    </div>
+    
+  )
+}
+
+export default App
